f7e9626a1c14aee565ca1b3e9b5ecbf566eae292
cloud <34809231+Purple-wind-bell@users.noreply.github.com>
2022-01-27 23:48:01 +0800

    Update cloudflare.sh

--- root/app/cloudflare.sh
+++ root/app/cloudflare.sh
@@ -21,50 +21,10 @@ cloudflare() {
   fi
 }
 
-getLocalIpAddress() {
-  if [ "$RRTYPE" == "A" ]; then
-    IP_ADDRESS=$(ip addr show $INTERFACE | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2; exit}')
-  elif [ "$RRTYPE" == "AAAA" ]; then
-    IP_ADDRESS=$(ip addr show $INTERFACE | awk '$1 == "inet6" {gsub(/\/.*$/, "", $2); print $2; exit}')
-  fi
-
+# 从result_hosts.txt中获取bestip
+getBestIpAddress() {
+    IP_ADDRESS=$(sed -n "2,1p" /CloudflareST_linux_amd64/result_hosts.txt | awk -F, '{print $1}')
   echo $IP_ADDRESS
-}
-
-getCustomIpAddress() {
-  IP_ADDRESS=$(sh -c "$CUSTOM_LOOKUP_CMD")
-  echo $IP_ADDRESS
-}
-
-getPublicIpAddress() {
-  if [ "$RRTYPE" == "A" ]; then
-    # Use DNS_SERVER ENV variable or default to 1.1.1.1
-    DNS_SERVER=${DNS_SERVER:=1.1.1.1}
-
-    # try dns method first.
-    CLOUD_FLARE_IP=$(dig +short @$DNS_SERVER ch txt whoami.cloudflare +time=3 | tr -d '"')
-    CLOUD_FLARE_IP_LEN=${#CLOUD_FLARE_IP}
-
-    # if using cloud flare fails, try opendns (some ISPs block 1.1.1.1)
-    IP_ADDRESS=$([ $CLOUD_FLARE_IP_LEN -gt 15 ] && echo $(dig +short myip.opendns.com @resolver1.opendns.com +time=3) || echo "$CLOUD_FLARE_IP")
-
-    # if dns method fails, use http method
-    if [ "$IP_ADDRESS" = "" ]; then
-      IP_ADDRESS=$(curl -sf4 https://ipinfo.io | jq -r '.ip')
-    fi
-
-    echo $IP_ADDRESS
-  elif [ "$RRTYPE" == "AAAA" ]; then
-    # try dns method first.
-    IP_ADDRESS=$(dig +short @2606:4700:4700::1111 -6 ch txt whoami.cloudflare | tr -d '"')
-
-    # if dns method fails, use http method
-    if [ "$IP_ADDRESS" = "" ]; then
-      IP_ADDRESS=$(curl -sf6 https://ifconfig.co)
-    fi
-
-    echo $IP_ADDRESS
-  fi
 }
 
 getDnsRecordName() {
