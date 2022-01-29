7fdc4180423e8239c3313a8fc841177f5635668d
cloud <34809231+Purple-wind-bell@users.noreply.github.com>
2022-01-29 20:45:02 +0800

    fix

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,4 @@
-git pull &&
-    docker build -t zfl666/cloudflare-bestip-ddns:latest . &&
-    docker push zfl666/cloudflare-bestip-ddns:latest
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+git pull
+docker build -t zfl666/cloudflare-bestip-ddns:latest .
+docker push zfl666/cloudflare-bestip-ddns:latest
